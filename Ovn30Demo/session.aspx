c1cfcfa9b8b03c58f8bce95265955511e0c97eb4
Patrik Jönsson <patrik@pattzor.se>
2017-10-16 09:48:43 +0200

    Some session demo

---
+++ Ovn30Demo/session.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/main.Master" AutoEventWireup="true" CodeBehind="session.aspx.cs" Inherits="Ovn30Demo.session" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="main_content" runat="server">
+    <asp:Label ID="Label1Info" runat="server" Text="Label" Font-Size="X-Large"></asp:Label>
+    <table style="width: 40%;">
+        <tr>
+            <td>Username</td>
+            <td>
+                <asp:TextBox ID="TextBoxUsername" runat="server"></asp:TextBox>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td>
+                <asp:TextBox ID="TextBoxPassword" runat="server"></asp:TextBox>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>
+                <asp:Button ID="Button1" runat="server" Text="Logga in" OnClick="Button1_Click" />&nbsp;</td>
+            <td>
+                <asp:Button ID="Button2" runat="server" Text="Logga ut" OnClick="Button2_Click" />&nbsp;</td>
+        </tr>
+    </table>
+</asp:Content>
---
+++ Ovn30Demo/session.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ovn30Demo
+{
+    public partial class session : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Session["user"] != null)
+            {
+                Label1Info.Text = "Logged in as " + Session["user"];
+            }
+            else
+            {
+                Label1Info.Text = "Logga in";
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string username = TextBoxUsername.Text;
+            string password = TextBoxPassword.Text;
+
+            if(username == "Kalle" && password == "Kula")
+            {
+                Session["user"] = "Kalle";
+
+                Label1Info.Text = "Logged in as Kalle";
+            }
+            else
+            {
+                Label1Info.Text = "Fel username/password";
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Session["user"] = null;
+            Label1Info.Text = "Logga in";
+        }
+    }
+}
